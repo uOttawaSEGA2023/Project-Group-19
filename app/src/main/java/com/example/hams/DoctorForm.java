a593d32927981fdd5e5c9e4db6d0fdd2f548d976
David Chen <dchen182@uottawa.ca>
2023-10-04 17:11:13 -0400

    UI for Doctor Form

---
+++ app/src/main/java/com/example/hams/DoctorForm.java
@@ -0,0 +1,14 @@
+package com.example.hams;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DoctorForm extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_doctor_form);
+    }
+}
