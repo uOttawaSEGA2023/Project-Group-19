e9e9bde4dacc34043adf9b823c920bb9073ba03d
David Chen <dchen182@uottawa.ca>
2023-10-05 19:08:03 -0400

    Fix Doctor Form Scrollview

--- app/src/main/java/com/example/hams/DoctorForm.java
+++ app/src/main/java/com/example/hams/DoctorForm.java
@@ -3,12 +3,60 @@ package com.example.hams;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Toast;
+
+
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class DoctorForm extends AppCompatActivity {
+
+    EditText firstName;
+    EditText lastName;
+    EditText email;
+    EditText password;
+    EditText phoneNumber;
+    EditText address;
+    EditText employeeNumber;
+    Button register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_doctor_form);
+
+        firstName = findViewById(R.id.firstName);
+        lastName = findViewById(R.id.lastName);
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+        phoneNumber = findViewById(R.id.phoneNumber);
+        address = findViewById(R.id.address);
+        employeeNumber = findViewById(R.id.employeeNumber);
+        register = findViewById(R.id.registerDoctor);
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
+
+    void validFields(){
+        if (fieldEmpty(firstName)){
+            firstName.setError("First name is required to register!");
+        }
+    }
+
+    boolean fieldEmpty(EditText text){
+        CharSequence entry = text.getText().toString();
+        return TextUtils.isEmpty(entry);
+    }
+
 }
