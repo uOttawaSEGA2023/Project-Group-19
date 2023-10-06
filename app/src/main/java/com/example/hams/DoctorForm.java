562e7116ba59fe53cb3016ba7f92ab4608d7aca7
David Chen <dchen182@uottawa.ca>
2023-10-06 11:26:40 -0400

    Scrolling and Address Fields added to Doctor Form

--- app/src/main/java/com/example/hams/DoctorForm.java
+++ app/src/main/java/com/example/hams/DoctorForm.java
@@ -1,8 +1,11 @@
 package com.example.hams;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -22,29 +25,47 @@ public class DoctorForm extends AppCompatActivity {
     EditText email;
     EditText password;
     EditText phoneNumber;
-    EditText address;
     EditText employeeNumber;
+    EditText addressLine;
+    EditText postalCode;
+    EditText country;
+    EditText province;
+    EditText city;
     Button register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_doctor_form);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle("HAMS - Doctor Registration Form");
+        actionBar.setDisplayHomeAsUpEnabled(true);
 
         firstName = findViewById(R.id.firstName);
         lastName = findViewById(R.id.lastName);
         email = findViewById(R.id.email);
         password = findViewById(R.id.password);
         phoneNumber = findViewById(R.id.phoneNumber);
-        address = findViewById(R.id.address);
         employeeNumber = findViewById(R.id.employeeNumber);
+
+        addressLine = findViewById(R.id.addressLine);
+        postalCode = findViewById(R.id.postalCode);
+        country = findViewById(R.id.country);
+        province = findViewById(R.id.province);
+        city = findViewById(R.id.city);
+
         register = findViewById(R.id.registerDoctor);
 
         register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) { //Method that processes code when the register button is pressed.
                 if (validFields()){ //If all the fields in the form are valid, do the following.
-
+                    String getFirstName = firstName.getText().toString();
+                    String getLastName = lastName.getText().toString();
+                    String getEmail = email.getText().toString();
+                    String getPassword = password.getText().toString();
+                    String getPhoneNumber = phoneNumber.getText().toString();
+                    String getEmployeeNumber = employeeNumber.getText().toString();
                 }
             }
         });
@@ -58,7 +79,7 @@ public class DoctorForm extends AppCompatActivity {
         boolean valid = true; //Variable to keep track of the current field validity.
         if (fieldEmpty(firstName)){  //If the first name field is empty, do the following.
             valid = false; //Set valid to false.
-            firstName.setError("A first name is required to register!");
+            firstName.setError("A first name is required to register!"); //Tell the user to enter something into the field.
         }
         if (fieldEmpty(lastName)){ //If the last name field is empty, do the following.
             valid = false; //Set valid to false.
@@ -76,15 +97,11 @@ public class DoctorForm extends AppCompatActivity {
             valid = false; //Set valid to false.
             phoneNumber.setError("A phone number is required to register!"); //Tell the user to enter something into the field.
         }
-        if (fieldEmpty(address)){ //If the address field is empty, do the following.
-            valid = false; //Set valid to false.
-            address.setError("An address is required to register!"); //Tell the user to enter something into the field.
-        }
+
         if (fieldEmpty(employeeNumber)){ //If the employee number field is empty, do the following.
             valid = false; //Set valid to false.
             employeeNumber.setError("An employee number is required to register!"); //Tell the user to enter something into the field.
         }
-
 
         CharSequence emailField = email.getText().toString(); //Gets the text from the email field.
         if (!Patterns.EMAIL_ADDRESS.matcher(emailField).matches()){ //If the email does not have the proper components of an email, do the following.
@@ -93,9 +110,21 @@ public class DoctorForm extends AppCompatActivity {
         }
 
         CharSequence phoneField = phoneNumber.getText().toString(); //Gets the text from the phone number field.
+        int length = 10; //Assume the phone number field will be 10 digits long.
+        for (int i = 0; i < phoneField.length(); i++){ //For loop to exclude - and spaces from the phone number field for checking.
+            if (phoneField.charAt(i) == '-' || phoneField.charAt(i) == ' '){
+                length--;
+            }
+        }
         if (phoneField.length() != 10){ //If the phone number is not equal to 10 digits, do the following.
             valid = false; //Set valid to false.
             phoneNumber.setError("A valid phone number need to be 10 digits long!"); //Tell the user to type in a phone number that is 10 digits long.
+        }
+
+        CharSequence passwordField = password.getText().toString(); //Gets the text from the password field.
+        if (passwordField.length() < 6){ //If the password is less than 6 characters long, do the following.
+            valid = false; //Set valid to false.
+            password.setError("A valid password needs to be at least 6 characters long!"); //Tell the user to type in a password that is at least 6 characters long.
         }
 
         return valid; //Returns whether or not all fields are valid.
@@ -109,6 +138,12 @@ public class DoctorForm extends AppCompatActivity {
     boolean fieldEmpty(EditText text){
         CharSequence entry = text.getText().toString();
         return TextUtils.isEmpty(entry);
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item){ //Method to enable the back button on the doctor form to return to the main menu.
+        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(intent);
+        return true;
     }
 
 }
