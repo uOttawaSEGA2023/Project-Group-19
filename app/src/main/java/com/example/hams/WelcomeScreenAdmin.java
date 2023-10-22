e7d106f2f02fb595e544d8548df9260971300ac9
David <dchen182@uottawa.ca>
2023-10-22 17:46:37 -0400

    Admin Welcome Screen Basic UI

--- app/src/main/java/com/example/hams/WelcomeScreenAdmin.java
+++ app/src/main/java/com/example/hams/WelcomeScreenAdmin.java
@@ -1,8 +1,12 @@
 package com.example.hams;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class WelcomeScreenAdmin extends AppCompatActivity {
 
@@ -10,5 +14,25 @@ public class WelcomeScreenAdmin extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome_screen_admin);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle("HAMS - Admin Account");
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        Button buttonLogin = findViewById(R.id.logoutAdmin);
+
+        buttonLogin.setOnClickListener(new View.OnClickListener(){
+            //Upon clicking the logout button sign user out and return to the login screen
+            public void onClick(View view){
+                openLoginScreen();
+            }
+        });
+    }
+
+    /**
+     * Opens the login screen.
+     */
+    public void openLoginScreen(){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
