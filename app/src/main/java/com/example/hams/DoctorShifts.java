e272771eaa4bbead128b970045cf153a6ef55898
arn477 <arnav.a.chaturvedi@gmail.com>
2023-11-14 00:41:48 -0500

    Displaying and removing shifts

--- app/src/main/java/com/example/hams/DoctorShifts.java
+++ app/src/main/java/com/example/hams/DoctorShifts.java
@@ -13,8 +13,12 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -26,6 +30,7 @@ public class DoctorShifts extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_doctor_shifts);
+
         String doctorUID = FirebaseAuth.getInstance().getCurrentUser().getUid();
 
         Button appointments = findViewById(R.id.backDoctor);
@@ -39,9 +44,31 @@ public class DoctorShifts extends AppCompatActivity {
         DatabaseReference ref = FirebaseDatabase.getInstance().getReference();
 
         ArrayList<Shift> shiftList = new ArrayList<>();
-        ShiftAdapter adapter = new ShiftAdapter(DoctorShifts.this, shiftList);
+        //ShiftAdapter adapter = new ShiftAdapter(DoctorShifts.this, shiftList);
         ListView listView = (ListView) findViewById(R.id.shiftsList);
-        listView.setAdapter(adapter);
+
+        ref.child("shifts").child(doctorUID).addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot snapshot, String previousChildName) {
+                Shift shift = snapshot.getValue(Shift.class);
+                shiftList.add(shift);
+
+                ShiftAdapter adapter = new ShiftAdapter(DoctorShifts.this, shiftList);
+                listView.setAdapter(adapter);
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot snapshot, String previousChildName) {}
+
+            @Override
+            public void onChildRemoved(DataSnapshot snapshot) {}
+
+            @Override
+            public void onChildMoved(DataSnapshot snapshot, String previousChildName) {}
+
+            @Override
+            public void onCancelled(DatabaseError error) {}
+        });
 
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() { //Code to run with a selected item from the shifts list.
             @Override
@@ -54,8 +81,9 @@ public class DoctorShifts extends AppCompatActivity {
                 deleteShift.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
-                        adapter.remove(shift);
-                        ref.child(doctorUID).child(shift.getKey()).removeValue();
+                        shiftList.remove(shift);
+                        listView.setAdapter(new ShiftAdapter(DoctorShifts.this, shiftList));
+                        ref.child("shifts").child(doctorUID).child(shift.getKey()).removeValue();
                         Toast.makeText(DoctorShifts.this, "Deleted Shift: " + shift.toString(), Toast.LENGTH_SHORT).show();
                     }
                 });
@@ -86,7 +114,6 @@ public class DoctorShifts extends AppCompatActivity {
 
                 shiftToAdd.setKey(newRef.getKey());
                 newRef.setValue(shiftToAdd);
-
             }
         });
     }
