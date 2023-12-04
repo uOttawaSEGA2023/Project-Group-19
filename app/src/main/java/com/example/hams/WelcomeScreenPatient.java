b0e1187a629b8386586f71f0fa2faa50486662ba
arn477 <arnav.a.chaturvedi@gmail.com>
2023-12-04 13:55:42 -0500

    Book appointment functionality

--- app/src/main/java/com/example/hams/WelcomeScreenPatient.java
+++ app/src/main/java/com/example/hams/WelcomeScreenPatient.java
@@ -11,10 +11,14 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -25,12 +29,17 @@ import com.google.firebase.database.ValueEventListener;
 
 import java.lang.reflect.Type;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class WelcomeScreenPatient extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     //calling objects needed for authentication and reading from the DB
     FirebaseAuth mAuth = FirebaseAuth.getInstance();
     DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference();
+    ListView slotsListView = (ListView) findViewById(R.id.appointmentSlots);
+    ArrayList<Appointment> appointmentList = new ArrayList<>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,13 +56,33 @@ public class WelcomeScreenPatient extends AppCompatActivity implements AdapterVi
 
         //reference to the dropdown
         Spinner spinner = findViewById(R.id.dropdown);
-
+        Button bookApp = findViewById(R.id.bookApp);
         //fills our dropdown with text
         ArrayAdapter<CharSequence> dropdownAdapter = ArrayAdapter.createFromResource(this, R.array.specializations, android.R.layout.simple_spinner_item);
         dropdownAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(dropdownAdapter);
 
         spinner.setOnItemSelectedListener(this);
+
+        slotsListView.setOnItemClickListener(new AdapterView.OnItemClickListener() { //Code to run with a selected item from the upcoming appointments list.
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Appointment appointment = (Appointment) parent.getItemAtPosition(position);
+
+                bookApp.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        String uID = mAuth.getCurrentUser().getUid();
+                        Map<String, Object> map = new HashMap<>();
+                        map.put("patientUID", uID);
+                        mDatabase.child("appointments").child(appointment.getKey()).updateChildren(map);
+                        //remove from list view.
+                        appointmentList.remove(appointment);
+                        slotsListView.setAdapter(new AppointmentAdapter(WelcomeScreenPatient.this, appointmentList));
+                    }
+                });
+            }
+        });
 
     }
 
@@ -63,11 +92,9 @@ public class WelcomeScreenPatient extends AppCompatActivity implements AdapterVi
         Toast.makeText(adapterView.getContext(), specialty, Toast.LENGTH_SHORT).show();
 
         ArrayList<Appointment> AppointmentSlots = new ArrayList<>();
-        ListView slotsListView = (ListView) findViewById(R.id.appointmentSlots);
 
         Query doctorQuery = mDatabase.child("users").orderByChild("type").equalTo("doctor");
         Query appointmentQuery = mDatabase.child("appointments").orderByChild("patientUID").equalTo("");
-        ArrayList<Appointment> appointmentList = new ArrayList<>();
 
         doctorQuery.addValueEventListener(new ValueEventListener() {
             @Override
