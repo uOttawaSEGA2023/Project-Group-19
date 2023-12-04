894d3dc9141aa9cba607c895689fe01627cff8d0
arn477 <arnav.a.chaturvedi@gmail.com>
2023-12-04 13:30:05 -0500

    search functionality

--- app/src/main/java/com/example/hams/WelcomeScreenPatient.java
+++ app/src/main/java/com/example/hams/WelcomeScreenPatient.java
@@ -1,5 +1,6 @@
 package com.example.hams;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -15,9 +16,12 @@ import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 import java.lang.reflect.Type;
 import java.util.ArrayList;
@@ -38,9 +42,6 @@ public class WelcomeScreenPatient extends AppCompatActivity implements AdapterVi
         ActionBar actionBar = getSupportActionBar();
         actionBar.setTitle("HAMS - Patient Screen: Appointments");
         actionBar.setDisplayHomeAsUpEnabled(true);
-        String chosenSpecialty = "";
-
-
 
         //setting up the dropdown of doctor specialities
 
@@ -58,36 +59,56 @@ public class WelcomeScreenPatient extends AppCompatActivity implements AdapterVi
 
     @Override
     public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-        String text = adapterView.getItemAtPosition(i).toString();
-        Toast.makeText(adapterView.getContext(), text, Toast.LENGTH_SHORT).show();
+        String specialty = adapterView.getItemAtPosition(i).toString();
+        Toast.makeText(adapterView.getContext(), specialty, Toast.LENGTH_SHORT).show();
 
         ArrayList<Appointment> AppointmentSlots = new ArrayList<>();
         ListView slotsListView = (ListView) findViewById(R.id.appointmentSlots);
 
         Query doctorQuery = mDatabase.child("users").orderByChild("type").equalTo("doctor");
+        Query appointmentQuery = mDatabase.child("appointments").orderByChild("patientUID").equalTo("");
+        ArrayList<Appointment> appointmentList = new ArrayList<>();
 
+        doctorQuery.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                ArrayList<String> doctorList = new ArrayList<>();
 
-        switch (text) {
-            case "Internal Medicine":
-                //display available slots for this specialization
+                for (DataSnapshot userSnapshot : snapshot.getChildren()) {
+                    Doctor doctor = userSnapshot.getValue(Doctor.class);
 
-                break;
-            case "Obstetrics":
+                    if (doctor.getSpecialties().contains(specialty)) {
+                        doctorList.add(doctor.getUserID());
+                    }
+                }
+                appointmentQuery.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
 
-                break;
-            case "Family Medicine":
+                        for (DataSnapshot userSnapshot : snapshot.getChildren()) {
+                            Appointment appointment = userSnapshot.getValue(Appointment.class);
 
-                break;
-            case "Gynecology":
+                            if (doctorList.contains(appointment.getDoctorUID())) {
+                                appointmentList.add(appointment);
+                            }
+                        }
+                        AppointmentAdapter adapter = new AppointmentAdapter(WelcomeScreenPatient.this, appointmentList);
 
-                break;
-            case "Pediatrics":
+                        slotsListView.setAdapter(adapter);
+                    }
 
-                break;
-            case "Neurology":
-                break;
-        }
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
 
+                    }
+                });
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 
     @Override
